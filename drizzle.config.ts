d93078998944383a6faf114a78f8d30116aadcd1
elisha <elishajameel2007@gmail.com>
2025-10-05 19:11:35 +0500

    added changes to drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/drizzle/schema.ts',
-  dialect: 'mysql',
+  out: ".src/drizzle/migration",
+  schema: "./src/drizzle/schema.ts",
+  dialect: "mysql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
