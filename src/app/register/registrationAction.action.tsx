6b7604f845ff5a4d360ef85042d0f3a408e147bb
elisha <elishajameel2007@gmail.com>
2025-10-10 12:18:51 +0500

    destructure the data

--- src/app/register/registrationAction.action.tsx
+++ src/app/register/registrationAction.action.tsx
@@ -1,6 +1,6 @@
-"use server"
+"use server";
 
-export const registrationAction = async (formData:FormData) => {    
-    console.log(Object.fromEntries(formData.entries()));
-    
-}
+export const registrationAction = async (formData: FormData) => {
+  const { name, userName, email, password, confirmPassword, role } =
+    Object.fromEntries(formData.entries());
+};
