0cc5c4c568616660cf0e0655b971beac4976c4ed
elisha <elishajameel2007@gmail.com>
2025-10-19 17:54:59 +0500

    prevent duplicate username and email

--- src/app/register/registrationAction.action.tsx
+++ src/app/register/registrationAction.action.tsx
@@ -3,6 +3,7 @@
 import { db } from "@/config/db";
 import { users } from "@/drizzle/schema";
 import argon2 from "argon2";
+import { eq, or } from "drizzle-orm";
 
 export const registrationAction = async (data: {
   name: string;
@@ -13,6 +14,22 @@ export const registrationAction = async (data: {
 }) => {
   try {
     const { name, userName, email, password, role } = data;
+
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(or(eq(users.email, email), eq(users.userName, userName)));
+
+    if (user) {
+      if (user.email === email)
+        return { status: "ERROR", message: "Email Already Exists" };
+      else
+        return {
+          status: "ERROR",
+          message: "Username already Exists",
+        };
+    }
+
     const hashPassword = await argon2.hash(password);
     await db
       .insert(users)
