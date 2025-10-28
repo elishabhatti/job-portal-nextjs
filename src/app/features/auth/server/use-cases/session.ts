bd4d46beaf7824de7b00230aa5ccfb43cd607ea3
elisha <elishajameel2007@gmail.com>
2025-10-28 13:14:27 +0500

    created function createUserSession and the type of that function

--- src/app/features/auth/server/use-cases/session.ts
+++ src/app/features/auth/server/use-cases/session.ts
@@ -2,9 +2,23 @@ import { headers } from "next/headers";
 import crypto from "crypto";
 import { getIpAddress } from "./location";
 
+type CreateSessionData = {
+  userAgent: string;
+  ip: string;
+  userId: number;
+  token: string;
+};
+
 const generateSessionToken = () => {
   return crypto.randomBytes(32).toString("hex").normalize();
 };
+
+export const createUserSession = async ({
+  token,
+  userId,
+  userAgent,
+  ip,
+}: CreateSessionData) => {};
 
 export const createSessionAnSetCookies = async (userId: number) => {
   const token = generateSessionToken();
