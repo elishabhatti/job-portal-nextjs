8e21f8de97510dcf7c6b72540d07d931d67f66a5
elisha <elishajameel2007@gmail.com>
2025-10-28 13:10:10 +0500

    made the function async

--- src/app/features/auth/server/use-cases/session.ts
+++ src/app/features/auth/server/use-cases/session.ts
@@ -1,13 +1,20 @@
 import { headers } from "next/headers";
-import crypto from "crypto"
+import crypto from "crypto";
 import { getIpAddress } from "./location";
 
 const generateSessionToken = () => {
-    return crypto.randomBytes(32).toString("hex").normalize()
-}
+  return crypto.randomBytes(32).toString("hex").normalize();
+};
 
-export const createSessionAnSetCookies = (userId: number) => {
-    const token = generateSessionToken()
-    const ip = getIpAddress();
-    // const headersList = await headers()     
+export const createSessionAnSetCookies = async (userId: number) => {
+  const token = generateSessionToken();
+  const ip = getIpAddress();
+  const headersList = await headers();
+
+  await createUserSession({
+    token,
+    userId: userId,
+    userAgent: headersList.get("user-agent") || "",
+    ip: ip,
+  });
 };
