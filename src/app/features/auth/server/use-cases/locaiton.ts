fc16da4b794b2c2f39e37b434997d54629859ea9
elisha <elishajameel2007@gmail.com>
2025-10-28 13:06:40 +0500

    created getIpAddress function

--- src/app/features/auth/server/use-cases/locaiton.ts
+++ src/app/features/auth/server/use-cases/locaiton.ts
@@ -1,11 +1,23 @@
 import { headers } from "next/headers";
 
 const IP_HEADERS_PRIORITY = [
-    "cf-connection-ip",
-    "x-client-ip",
-    "x-forwarded-for",
-    "x-real-ip",
-    "x-cluster-client-ip",
-    "forwarded-for",
-    "forwarded",
-]
+  "cf-connection-ip",
+  "x-client-ip",
+  "x-forwarded-for",
+  "x-real-ip",
+  "x-cluster-client-ip",
+  "forwarded-for",
+  "forwarded",
+];
+
+export async function getIpAddress() {
+  const headersList = await headers();
+  for (const header of IP_HEADERS_PRIORITY) {
+    const value = headersList.get(header);
+    if (typeof value === "string") {
+      const ip = value.split(",")[0].trim();
+      if (ip) return ip;
+    }
+  }
+  return "0.0.0.0";
+}
--- src/app/features/auth/server/use-cases/session.ts
+++ src/app/features/auth/server/use-cases/session.ts
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import crypto from "crypto"
+import { getIpAddress } from "./locaiton";
 
 const generateSessionToken = () => {
     return crypto.randomBytes(32).toString("hex").normalize()
