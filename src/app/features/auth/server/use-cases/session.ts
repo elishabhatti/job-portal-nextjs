d0ac4e75ad35ed6339cddb4ca0eb86e256fd5d4a
elisha <elishajameel2007@gmail.com>
2025-10-28 12:56:23 +0500

    create a token when createSessionAnSetCookies

--- src/app/features/auth/server/use-cases/session.ts
+++ src/app/features/auth/server/use-cases/session.ts
@@ -1,3 +1,12 @@
+import { headers } from "next/headers";
+import crypto from "crypto"
+
+const generateSessionToken = () => {
+    return crypto.randomBytes(32).toString("hex").normalize()
+}
+
 export const createSessionAnSetCookies = (userId: number) => {
-    
+    const token = generateSessionToken()
+    // const ip =
+    // const headersList = await headers()     
 };
