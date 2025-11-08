722dcc9293c41531a67255aa3bdf731a3d75a9a9
elisha <elishajameel2007@gmail.com>
2025-11-08 09:08:55 +0500

    added root layout

--- src/app/employer-dashboard/layout.tsx
+++ src/app/employer-dashboard/layout.tsx
@@ -1,7 +1,9 @@
-export default function RootLayout({
+// import {getCurrentUser} from ""
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+//   const user = await getCurrentUser();
   return { children };
 }
