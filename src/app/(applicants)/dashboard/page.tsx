cec9477dc99e850d53fe5734d2b416002c71da79
elisha <elishajameel2007@gmail.com>
2025-11-12 09:22:09 +0500

    added logout btn in applicant dashboard

--- src/app/(applicants)/dashboard/page.tsx
+++ src/app/(applicants)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { logoutUserAction } from "@/app/features/auth/server/auth.action";
+
 const Applicant = () => {
   return (
     <div>
       <h1>Hello Applicant Dashboard.</h1>
+      <button onClick={logoutUserAction}>Logout</button>
     </div>
   );
 };
