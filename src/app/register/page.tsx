e59f0129c8ca81b1bafd726623a611854f009003
elisha <elishajameel2007@gmail.com>
2025-11-13 15:25:15 +0500

    added get user and added there imports

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import RegistrationForm from "../features/auth/components/register-form";
+import { getCurrentUser } from "../features/auth/server/auth.quires";
 
-const Registration = () => {
+const Registration = async () => {
+    const user = await getCurrentUser();
+    console.log("user", user);
+  
+    if (!user) return redirect("/login");
+    if (user.role !== "employer") return redirect("/dashboard");
   return (
     <>
       <RegistrationForm />
