a8c3946fa4065b6982568c5315c5d6907d4d943a
elisha <elishajameel2007@gmail.com>
2025-11-13 15:26:38 +0500

    redirect user by their roles

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,11 +3,15 @@ import RegistrationForm from "../features/auth/components/register-form";
 import { getCurrentUser } from "../features/auth/server/auth.quires";
 
 const Registration = async () => {
-    const user = await getCurrentUser();
-    console.log("user", user);
-  
-    if (!user) return redirect("/login");
-    if (user.role !== "employer") return redirect("/dashboard");
+  const user = await getCurrentUser();
+  console.log("user", user);
+
+  if (!user) return redirect("/login");
+
+  if (user) {
+    if (user.role === "applicant") return redirect("/dashboard");
+    if (user.role === "employer") return redirect("/employer-dashboard");
+  }
   return (
     <>
       <RegistrationForm />
