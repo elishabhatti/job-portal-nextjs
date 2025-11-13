9d0452a6034f114fb399b7733bd1330355c9811d
elisha <elishajameel2007@gmail.com>
2025-11-13 16:03:59 +0500

    added styling in the employer sidebar

--- src/app/features/employers/components/employer-sidebar.tsx
+++ src/app/features/employers/components/employer-sidebar.tsx
@@ -1,40 +1,64 @@
 import {
-  Bookmark,
-  Briefcase,
-  Building,
-  CreditCard,
   LayoutDashboard,
-  Plus,
-  Settings,
   User,
+  Plus,
+  Briefcase,
+  Bookmark,
+  CreditCard,
+  Building,
+  Settings,
+  LogOut,
 } from "lucide-react";
 import Link from "next/link";
+import { logoutUserAction } from "../../auth/server/auth.action";
 
 const base = "/employer-dashboard";
 
 const navigationItems = [
   { name: "Overview", icon: LayoutDashboard, href: base + "/" },
-  { name: "Employer Profile", icon: User, href: base + "/" },
-  { name: "Post a Job", icon: Plus, href: base + "/" },
-  { name: "My Jobs", icon: Briefcase, href: base + "/" },
-  { name: "Saved Candidate", icon: Bookmark, href: base + "/" },
-  { name: "Plans & Billing", icon: CreditCard, href: base + "/" },
-  { name: "All Companies", icon: Building, href: base + "/" },
+  { name: "Employers Profile", icon: User },
+  { name: "Post a Job", icon: Plus },
+  { name: "My Jobs", icon: Briefcase },
+  { name: "Saved Candidate", icon: Bookmark },
+  { name: "Plans & Billing", icon: CreditCard },
+  { name: "All Companies", icon: Building },
   { name: "Settings", icon: Settings, href: base + "/settings" },
 ];
 
 const EmployerSidebar = () => {
   return (
-    <div>
-      {navigationItems.map((currNav) => {
-        const Icon = currNav.icon;
-        return (
-          <Link key={currNav.name} href={currNav.href}>
-            <Icon />
-            {currNav.name}
-          </Link>
-        );
-      })}
+    <div className="w-64 bg-card border-r border-border fixed bottom-0 top-0">
+      <div className="p-6">
+        <h2 className="text-sm font-medium text-muted-foreground uppercase tracking-wide">
+          Employers Dashboard
+        </h2>
+      </div>
+
+      <nav className="px-3 space-y-1">
+        {navigationItems.map((curNav) => {
+          const Icon = curNav.icon;
+          return (
+            <Link
+              key={curNav.name}
+              href={curNav.href || "#"}
+              className=" flex items-center gap-3 px-3 py-2 text-sm font-medium rounded-lg transition-colors"
+            >
+              <Icon />
+              {curNav.name}
+            </Link>
+          );
+        })}
+      </nav>
+
+      <div className="absolute bottom-6 left-3 right-3">
+        <button
+          onClick={logoutUserAction}
+          className="flex items-center gap-3 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground hover:bg-accent rounded-lg transition-colors w-full"
+        >
+          <LogOut className="h-4 w-4" />
+          Log-out
+        </button>
+      </div>
     </div>
   );
 };
