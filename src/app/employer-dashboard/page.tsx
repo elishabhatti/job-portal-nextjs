fba657221d97ea1863dc885cfa16a55fc94f72b7
elisha <elishajameel2007@gmail.com>
2025-11-13 16:26:36 +0500

    change the employer dashboard

--- src/app/employer-dashboard/page.tsx
+++ src/app/employer-dashboard/page.tsx
@@ -1,11 +1,27 @@
-import { logoutUserAction } from "../features/auth/server/auth.action";
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "../features/auth/server/auth.quires";
 
-const EmployerDashboard = () => {
+const EmployerDashboard = async () => {
+  const user = await getCurrentUser();
+  console.log("user data employer: ", user);
+
+  if (!user) return redirect("/login");
+
   return (
-    <div>
-      <h1>Welcome, Employer</h1>
+    <div className="space-y-6">
+      {/* Header */}
+      <div>
+        <h1 className="text-2xl font-semibold text-foreground">
+          Hello, <span className="capitalize">{user?.name.toLowerCase()}</span>
+        </h1>
+        <p className="text-muted-foreground">
+          Here is your daily activities and appLications
+        </p>
+      </div>
+
+      {/* Stats Cards */}
+      {/* <StatsCards /> */}
     </div>
   );
 };
-
 export default EmployerDashboard;
