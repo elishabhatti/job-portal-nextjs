56874c2b40bfa78d0611b53427d76863fdce8ee9
elisha <elishajameel2007@gmail.com>
2025-11-13 15:54:50 +0500

    added navigation Items with their icons

--- src/app/features/employers/components/employer-sidebar.tsx
+++ src/app/features/employers/components/employer-sidebar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {
+  Bookmark,
+  Briefcase,
+  Building,
+  CreditCard,
+  LayoutDashboard,
+  Plus,
+  Settings,
+  User,
+} from "lucide-react";
+
+const navigationItems = [
+  { name: "Overview", icon: LayoutDashboard, href: base + "/" },
+  { name: "Employer Profile", icon: User, href: base + "/" },
+  { name: "Post a Job", icon: Plus, href: base + "/" },
+  { name: "My Jobs", icon: Briefcase, href: base + "/" },
+  { name: "Saved Candidate", icon: Bookmark, href: base + "/" },
+  { name: "Plans & Billing", icon: CreditCard, href: base + "/" },
+  { name: "All Companies", icon: Building, href: base + "/" },
+  { name: "Settings", icon: Settings, href: base + "/settings" },
+];
 
 const EmployerSidebar = () => {
-  return (
-    <div>EmployerSidebar</div>
-  )
-}
+  return <div>EmployerSidebar</div>;
+};
 
-export default EmployerSidebar
+export default EmployerSidebar;
