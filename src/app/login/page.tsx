f1451c798748363925bc354005598f1310558a59
elisha <elishajameel2007@gmail.com>
2025-11-13 15:34:25 +0500

    removed logic if the user is not found

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,6 @@ import { getCurrentUser } from "../features/auth/server/auth.quires";
 
 const Login = async () => {
   const user = await getCurrentUser();
-  if (!user) return redirect("/login");
-
   if (user) {
     if (user.role === "applicant") return redirect("/dashboard");
     if (user.role === "employer") return redirect("/employer-dashboard");
