9d3fd18ea380d76c1d072a774e305f31745828e1
elisha <elishajameel2007@gmail.com>
2025-11-16 21:19:21 +0500

    getting the isProfileCompleted and returining it

--- src/app/features/server/employers.queries.ts
+++ src/app/features/server/employers.queries.ts
@@ -1,19 +1,28 @@
 import { db } from "@/config/db";
-// import { getCurrentUser } from "../auth/server/auth.quires";
-
 import { employers } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
 import { getCurrentUser } from "../auth/server/auth.querie";
 
 export const getCurrentEmployerDetails = async () => {
   const currentUser = await getCurrentUser();
+  console.log("user data employer: ", currentUser);
 
-  if (!currentUser || currentUser.role !== "employer") return null;
+  if (!currentUser) return null;
+  if (currentUser.role !== "employer") return null;
 
   const [employer] = await db
     .select()
     .from(employers)
     .where(eq(employers.id, currentUser.id));
 
-  return employer ?? null;
+  console.log("employer: ", employer);
+
+  const isProfileCompleted =
+    employer.name &&
+    employer.description &&
+    employer.avatarUrl &&
+    employer.organizationType &&
+    employer.yearOfEstablishment;
+
+  return { ...currentUser, employerDetails: employer, isProfileCompleted };
 };
