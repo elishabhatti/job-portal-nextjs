db037f92c2b40336c835195ac68579b4066b3e52
elisha <elishajameel2007@gmail.com>
2025-11-16 21:06:05 +0500

    getting the current employer

--- src/app/features/server/employers.queries.ts
+++ src/app/features/server/employers.queries.ts
@@ -1,9 +1,19 @@
-import { redirect } from "next/navigation";
+import { db } from "@/config/db";
 import { getCurrentUser } from "../auth/server/auth.quires";
+import { employers } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
 
 export const getCurrentEmployerDetails = async () => {
   const currentUser = await getCurrentUser();
   console.log("user data employer: ", currentUser);
 
-  if (!currentUser) return redirect("/login");
+  if (!currentUser) return null;
+  if (currentUser.role !== "employer") return null;
+
+  const [employer] = await db
+    .select()
+    .from(employers)
+    .where(eq(employers.id, currentUser.id));
+
+  console.log("employer: ", employer);
 };
