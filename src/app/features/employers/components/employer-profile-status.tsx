2e546f14251f6e66db50c706b39c300f8258a05e
elisha <elishajameel2007@gmail.com>
2025-11-16 20:54:10 +0500

    showed warning message in with shadcn

--- src/app/features/employers/components/employer-profile-status.tsx
+++ src/app/features/employers/components/employer-profile-status.tsx
@@ -1,12 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { ShieldAlertIcon } from "lucide-react";
+import Link from "next/link";
 
 const EmployerProfileCompletionStatus = () => {
   return (
     <div className="flex flex-col gap-6">
-        {/* <Item variant="destructive">
-
-        </Item> */}
+      <Item variant="destructive">
+        <ItemMedia variant="icon" className="bg-destructive">
+          <ShieldAlertIcon className="text-white" />
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle className="text-white">Incomplete Profile</ItemTitle>
+          <ItemDescription className="text-white/80">
+            You haven't completed your employer profile yet. Please complete
+            your profile to post jobs and access all features.
+          </ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button size="sm" variant="destructive" asChild>
+            <Link href="/employer-dashboard/settings">Complete Profile</Link>
+          </Button>
+        </ItemActions>
+      </Item>
     </div>
-  )
-}
+  );
+};
 
-export default EmployerProfileCompletionStatus
+export default EmployerProfileCompletionStatus;
