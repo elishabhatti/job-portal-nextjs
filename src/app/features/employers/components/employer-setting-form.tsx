7870f59bca9b6d4760cbb7e40f44dd6dc75cbc17
elisha <elishajameel2007@gmail.com>
2025-11-22 09:18:43 +0500

    use used react form hook for getting and submitting data

--- src/app/features/employers/components/employer-setting-form.tsx
+++ src/app/features/employers/components/employer-setting-form.tsx
@@ -1,15 +1,33 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+  username: string;
+  email: string;
+}
 
 const EmployerSettingsForm = () => {
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const handleFormSubmit = (data: IFormInput) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
         <div className="grid w-full max-w-sm items-center gap-3">
-          <Label htmlFor="picture">Picture</Label>
-          <Input id="picture" type="file" />
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" type="file" {...register("username")} />
         </div>
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="file" {...register("email")} />
+        </div>
+        <Button type="submit">Save Changes</Button>
       </form>
     </div>
   );
