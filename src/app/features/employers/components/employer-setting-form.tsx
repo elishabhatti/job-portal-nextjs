8f4dda52bd75364f5410b77b0210c7f57853d4a7
elisha <elishajameel2007@gmail.com>
2025-11-22 09:11:51 +0500

    added input and label from shadcn

--- src/app/features/employers/components/employer-setting-form.tsx
+++ src/app/features/employers/components/employer-setting-form.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
 
 const EmployerSettingsForm = () => {
   return (
-    <div>EmployerSettingsForm</div>
-  )
-}
+    <div>
+      <form>
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Label htmlFor="picture">Picture</Label>
+          <Input id="picture" type="file" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default EmployerSettingsForm
+export default EmployerSettingsForm;
