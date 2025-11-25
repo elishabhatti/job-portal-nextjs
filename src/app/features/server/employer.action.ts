55b9ee752db6da5e730510f563501977fb7d6d71
elisha <elishajameel2007@gmail.com>
2025-11-25 16:12:58 +0500

    getting the data

--- src/app/features/server/employer.action.ts
+++ src/app/features/server/employer.action.ts
@@ -1,11 +1,13 @@
+import { db } from "@/config/db";
+import { getCurrentUser } from "../auth/server/auth.quires";
+import { employers } from "@/drizzle/schema";
+
 const organizationTypeOptions = ["development", "business", "design"] as const;
 type OrganizationType = (typeof organizationTypeOptions)[number];
 const teamSizeOptions = ["1-5", "6-20", "21-50"] as const;
 type TeamSize = (typeof teamSizeOptions)[number];
 
 interface IFormInput {
-  username: string;
-  email: string;
   name: string;
   description: string;
   yearOfEstablishment: string;
@@ -15,4 +17,32 @@ interface IFormInput {
   teamSize: TeamSize;
 }
 
-export const updateEmployerProfileAction = (data: IFormInput) => {};
+export const updateEmployerProfileAction = async (data: IFormInput) => {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser || currentUser.role !== "employer") {
+      return { status: "ERROR", message: "Unauthorized" };
+    }
+    const {
+      name,
+      description,
+      yearOfEstablishment,
+      location,
+      websiteUrl,
+      organizationType,
+      teamSize,
+    } = data;
+
+    await db.update(employers).set({
+      name,
+      description,
+      location,
+      websiteUrl,
+      organizationType,
+      teamSize,
+      yearOfEstablishment: yearOfEstablishment
+        ? parseInt(yearOfEstablishment)
+        : null,
+    });
+  } catch (error) {}
+};
