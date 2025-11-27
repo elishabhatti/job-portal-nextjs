d390b793b15f35c665c1d024f71d199f0d9b3fb6
elisha <elishajameel2007@gmail.com>
2025-11-27 18:30:42 +0500

    call the getCurrentEmployerDetails() to get the details of the employer in the settings page

--- src/app/employer-dashboard/settings/page.tsx
+++ src/app/employer-dashboard/settings/page.tsx
@@ -1,6 +1,10 @@
 import EmployerSettingsForm from "@/app/features/employers/components/employer-setting-form";
+import { getCurrentEmployerDetails } from "@/app/features/server/employers.queries";
 
-const EmployerSettings = () => {
+const EmployerSettings = async () => {
+  const currentEmployer = await getCurrentEmployerDetails();
+  console.log(currentEmployer);
+
   return (
     <div>
       <EmployerSettingsForm />
