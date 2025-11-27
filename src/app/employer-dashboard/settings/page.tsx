3f92dd844b7647144e7cfbf2a16a13a24f3e2caf
elisha <elishajameel2007@gmail.com>
2025-11-27 18:31:38 +0500

    if currentEmployer not found return to login

--- src/app/employer-dashboard/settings/page.tsx
+++ src/app/employer-dashboard/settings/page.tsx
@@ -1,8 +1,10 @@
 import EmployerSettingsForm from "@/app/features/employers/components/employer-setting-form";
 import { getCurrentEmployerDetails } from "@/app/features/server/employers.queries";
+import { redirect } from "next/navigation";
 
 const EmployerSettings = async () => {
   const currentEmployer = await getCurrentEmployerDetails();
+  if (!currentEmployer) return redirect("/login");
   console.log(currentEmployer);
 
   return (
