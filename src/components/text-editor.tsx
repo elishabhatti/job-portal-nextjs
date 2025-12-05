61d46458c93631118fb73d7854c4ce9d4e069612
elisha <elishajameel2007@gmail.com>
2025-12-05 10:02:19 +0500

    created component tool bar added bold btn

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,22 +1,37 @@
-"use client"
+"use client";
 // src/Tiptap.tsx
-import { useEditor, EditorContent } from '@tiptap/react'
+import { useEditor, EditorContent, Editor, useEditorState } from "@tiptap/react";
 // import { FloatingMenu, BubbleMenu } from '@tiptap/react/menus'
-import StarterKit from '@tiptap/starter-kit'
+import StarterKit from "@tiptap/starter-kit";
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [StarterKit], // define your extension array
-    content: '<p>Hello World!</p>', // initial content
-  })
+    content: "<p>Hello World!</p>", // initial content
+    immediatelyRender: false,
+  });
 
   return (
     <>
+      {editor && <ToolBar editor={editor} />}
       <EditorContent editor={editor} />
       {/* <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu> */}
       {/* <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu> */}
     </>
-  )
-}
+  );
+};
 
-export default Tiptap
+export default Tiptap;
+
+const ToolBar = ({ editor }: { editor: Editor }) => {
+    const editorState = useEditorState({editor, selector(context) {} })
+  return (
+    <button
+      onClick={() => editor.chain().focus().toggleBold().run()}
+      disabled={!editorState.canBold}
+      className={editorState.isBold ? "is-active" : ""}
+    >
+      Bold
+    </button>
+  );
+};
