da836668caf0f9229e114413e3627f1a40021513
elishabhatti <elishajameel2007@gmail.com>
2025-12-13 09:23:54 +0500

    added avatar url in settings file

--- src/app/employer-dashboard/settings/page.tsx
+++ src/app/employer-dashboard/settings/page.tsx
@@ -1,25 +1,30 @@
 import EmployerSettingsForm from "@/app/features/employers/components/employer-setting-form";
+import { EmployerProfileData } from "@/app/features/employers/employer.schema";
 import { getCurrentEmployerDetails } from "@/app/features/server/employers.queries";
 import { redirect } from "next/navigation";
 
 const EmployerSettings = async () => {
   const employer = await getCurrentEmployerDetails();
   if (!employer) return redirect("/login");
-  console.log("current employer:", employer);
+
+  console.log("currentEmployer: ", employer);
 
   return (
     <div>
       <EmployerSettingsForm
-        initialData={{
-          name: employer.employerDetails.name,
-          description: employer.employerDetails.description,
-          organizationType: employer.employerDetails.organizationType,
-          teamSize: employer.employerDetails.teamSize,
-          location: employer.employerDetails.location,
-          websiteUrl: employer.employerDetails.websiteUrl,
-          yearOfEstablishment:
-            employer.employerDetails.yearOfEstablishment?.toString(),
-        }}
+        initialData={
+          {
+            name: employer.employerDetails.name,
+            description: employer.employerDetails.description,
+            organizationType: employer.employerDetails.organizationType,
+            teamSize: employer.employerDetails.teamSize,
+            location: employer.employerDetails.location,
+            websiteUrl: employer.employerDetails.websiteUrl,
+            yearOfEstablishment:
+              employer.employerDetails.yearOfEstablishment?.toString(),
+            avatarUrl: employer.avatarUrl,
+          } as EmployerProfileData
+        }
       />
     </div>
   );
