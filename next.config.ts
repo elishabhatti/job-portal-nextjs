a5cb7852a5c7ea8deb01068b44ce27c044f4e7a8
elishabhatti <elishajameel2007@gmail.com>
2025-12-13 09:54:45 +0500

    added image config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "sxhrylj45n.ufs.sh",
+        protocol: "https",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
