439b2d6a81061cc631e18aa27cd16ab4d24f7fcf
elisha <elishajameel2007@gmail.com>
2026-01-09 10:12:11 +0500

    use jobs in employer job list

--- src/app/features/employers/components/employer-job-list.tsx
+++ src/app/features/employers/components/employer-job-list.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import { Job } from "../jobs/types/job.types";
 
 const EmployerJobList = () => {
   const [jobs, setJobs] = useState<Job[]>([]);
