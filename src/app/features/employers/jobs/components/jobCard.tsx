366177ca829aa172e782b3d19fc11feb4460b597
elisha <elishajameel2007@gmail.com>
2026-01-27 17:11:21 +0500

    made the getall jobs function

--- src/app/features/employers/jobs/components/jobCard.tsx
+++ src/app/features/employers/jobs/components/jobCard.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+import { JobCardType } from "../server/jobs.queries";
 
 interface JobCardProps {
   job: JobCardType;
@@ -9,12 +11,44 @@ const JobCard = ({ job }: JobCardProps) => {
     if (!job.minSalary || !job.maxSalary) return "Not Disclosed";
     return `${job.salaryCurrency} ${job.minSalary.toLocalString()} - ${job.maxSalary.toLocalString()}`;
   };
-  
+
   return (
     <div>
-      <Link href={`/dashboard/jobs/${job.id}`} className="group flex flex-col gap-4 rounded-xl border border-gray-200 bg"></Link>
+      <Link
+        href={`/dashboard/jobs/${job.id}`}
+        className="group flex flex-col gap-4 rounded-xl border border-gray-200 bg-white p-5 shadow-sm transition-all hover:border-blue-500/50 hover:shadow-md"
+      >
+        {/* Header Logo & Title */}
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex gap-3">
+            <div className="relative h-12 w-12 shrink-0 overflow-hidden rounded-lg border border-gray-100 bg-gray-50">
+              {job.companyLogo ? (
+                <Image
+                  src={job.companyLogo}
+                  alt={job.companyName || "Company Logo"}
+                  fill
+                  className="object-cover"
+                />
+              ) : (
+                <div className="flex h-full w-full items-center justify-center bg-gray-100 font-bold text-gray-400">
+                  {job.companyName?.slice(0, 2).toUpperCase() || "CO"}
+                </div>
+              )}
+            </div>
+
+            <div>
+              <h3 className="max-w-45 truncate font-semibold text-gray-900 group-hover:text-blue-600">
+                {job.title}
+              </h3>
+              <p className="text-sm text-gray-500">{job.companyName}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Badge */}
+      </Link>
     </div>
-  )
+  );
 };
 
 export default JobCard;
--- src/app/features/employers/jobs/server/jobs.queries.ts
+++ src/app/features/employers/jobs/server/jobs.queries.ts
@@ -2,32 +2,37 @@ import { db } from "@/config/db";
 import { employers, jobs, users } from "@/drizzle/schema";
 import { and, desc, eq, gte, isNull, or } from "drizzle-orm";
 
-const jobsData = await db
-  .select({
-    id: jobs.id,
-    title: jobs.title,
-    description: jobs.description,
-    minSalary: jobs.minSalary,
-    maxSalary: jobs.maxSalary,
-    salaryCurrency: jobs.salaryCurrency,
-    salaryPeriod: jobs.salaryPeriod,
-    location: jobs.location,
-    jobType: jobs.jobType,
-    workType: jobs.workType,
-    createdAt: jobs.createdAt,
-    companyName: employers.name,
-    companyLogo: users.avatarUrl,
-  })
-  .from(jobs)
-  .innerJoin(employers, eq(jobs.employerId, employers.id))
-  .innerJoin(users, eq(employers.id, users.id))
-  .where(
-    and(
+export const getAllJobs = async () => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const jobsData = await db
+    .select({
+      id: jobs.id,
+      title: jobs.title,
+      description: jobs.description,
+      minSalary: jobs.minSalary,
+      maxSalary: jobs.maxSalary,
+      salaryCurrency: jobs.salaryCurrency,
+      salaryPeriod: jobs.salaryPeriod,
+      location: jobs.location,
+      jobType: jobs.jobType,
+      workType: jobs.workType,
+      createdAt: jobs.createdAt,
+      companyName: employers.name,
+      companyLogo: users.avatarUrl,
+    })
+    .from(jobs)
+    .innerJoin(employers, eq(jobs.employerId, employers.id))
+    .innerJoin(users, eq(employers.id, users.id))
+    .where(
+      and(
         isNull(jobs.deletedAt),
         or(isNull(jobs.expiresAt), gte(jobs.expiresAt, today)),
+      ),
     )
-  )
-  .orderBy(desc(jobs.createdAt));
+    .orderBy(desc(jobs.createdAt));
   return jobsData;
+};
 
-  export type JobCardType = Awaited<ReturnType<typeof getAllJobs>>[number];
+export type JobCardType = Awaited<ReturnType<typeof getAllJobs>>[number];
