7306ef65c3a135fe442c470ccdfac8eefc73073d
elisha <elishajameel2007@gmail.com>
2026-01-30 16:23:15 +0500

    quering full job location

--- src/app/features/employers/jobs/server/jobs.queries.ts
+++ src/app/features/employers/jobs/server/jobs.queries.ts
@@ -37,7 +37,45 @@ export const getAllJobs = async () => {
 
 export type JobCardType = Awaited<ReturnType<typeof getAllJobs>>[number];
 
+export const getJobById = async (jobId: number) => {
+  const job = await db
+    .select({
+      // Basic Info
+      id: jobs.id,
+      title: jobs.title,
+      description: jobs.description,
+      tags: jobs.tags,
 
-export const getJobById = (jobId: number) => {
+      // Salary Details
+      minSalary: jobs.minSalary,
+      maxSalary: jobs.maxSalary,
+      salaryCurrency: jobs.salaryCurrency,
+      salaryPeriod: jobs.salaryPeriod,
 
-}
+      // Job Meta Data
+      location: jobs.location,
+      jobType: jobs.jobType,
+      workType: jobs.workType,
+      jobLevel: jobs.jobLevel,
+      experience: jobs.experience,
+      minEducation: jobs.minEducation,
+
+      // Timestamps
+      createdAt: jobs.createdAt,
+      expiresAt: jobs.expiresAt,
+
+      // Employers Info
+      companyLogo: users.avatarUrl,
+      companyName: employers.name,
+      companyBio: employers.description,
+      companyWebsite: employers.websiteUrl,
+      companyLocation: employers.location,
+    })
+    .from(jobs)
+    .innerJoin(employers, eq(jobs.employerId, employers.id))
+    .innerJoin(users, eq(employers.id, users.id))
+    .where(eq(jobs.id, jobId))
+    .limit(1);
+
+  return job[0];
+};
