5faef77a1432ae5e5bed6c285ebbed3d7437aab2
elisha <elishajameel2007@gmail.com>
2026-02-01 21:47:48 +0500

    created file for job overview

---
+++ src/app/features/applicants/components/JobOverviewSidebar.tsx
@@ -0,0 +1,10 @@
+interface JobSideBarProps {
+    job: NonNullable<JobDetailsType>;
+}
+const JobOverviewSidebar = () => {
+  return (
+    <div>JobOverviewSidebar</div>
+  )
+}
+
+export default JobOverviewSidebar
