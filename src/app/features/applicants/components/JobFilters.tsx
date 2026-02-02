e9eb8e72edd43f72feca1e03d5b23adc7e00a855
elisha <elishajameel2007@gmail.com>
2026-02-02 18:21:36 +0500

    fix bug made the componet client side

--- src/app/features/applicants/components/JobFilters.tsx
+++ src/app/features/applicants/components/JobFilters.tsx
@@ -1,26 +1,34 @@
-import { useRouter, useSearchParams } from "next/navigation"
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const JobFilters = () => {
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    console.log("searchParams: ", searchParams);
-    console.log("searchingParams string: ", searchParams.toString());    
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  console.log("searchParams: ", searchParams);
+  console.log("searchingParams string: ", searchParams.toString());
 
-    const [search, setSearch] = useState(searchParams.get("search") || "");
-    const [jobType, setJobType] = useState(searchParams.get("jobType") || "");
-    const [jobLevel, setJobLevel] = useState(searchParams.get("jobLevel") || "");
-    const [workType, setWorkType] = useState(searchParams.get("workType") || "");
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [jobType, setJobType] = useState(searchParams.get("jobType") || "");
+  const [jobLevel, setJobLevel] = useState(searchParams.get("jobLevel") || "");
+  const [workType, setWorkType] = useState(searchParams.get("workType") || "");
 
-    useEffect(() => {
+  useEffect(() => {}, [search]);
 
-    },[search])
+  const updateFilters = (newParams: Record<string, string | null>) => {};
 
-    const updateFilters = (newParams: Record<string, string | null>) => {}
-    
-  return (
-    <div>JobFilters</div>
-  )
-}
+  const clearFilters = () => {
+    setSearch("");
+    setJobType("");
+    setJobLevel("");
+    setWorkType("");
 
-export default JobFilters
+    const pathname = "/dashboard/jobs";
+    router.push(pathname); // reset the base url
+  }
+
+  return <div>JobFilters</div>;
+};
+
+export default JobFilters;
