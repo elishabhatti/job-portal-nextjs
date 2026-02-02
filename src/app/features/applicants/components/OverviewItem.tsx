4a6a6dee51f689b63dc364c6c2f56590763d5397
elisha <elishajameel2007@gmail.com>
2026-02-02 09:37:12 +0500

    added about the company

--- src/app/features/applicants/components/OverviewItem.tsx
+++ src/app/features/applicants/components/OverviewItem.tsx
@@ -1,7 +1,14 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { JobDetailsType } from "../../employers/jobs/server/jobs.queries";
-import { Banknote, Briefcase, CalendarDays, GraduationCap } from "lucide-react";
+import {
+  Banknote,
+  Briefcase,
+  CalendarDays,
+  Globe,
+  GraduationCap,
+} from "lucide-react";
 import OverviewItem from "./jobOverViewSidebar";
+import Link from "next/link";
 
 interface JobSideBarProps {
   job: NonNullable<JobDetailsType>;
@@ -45,6 +52,34 @@ const JobOverviewSidebar = ({ job }: JobSideBarProps) => {
               label="Education"
               value={job.minEducation?.replace("-", "")}
             />
+          </CardContent>
+        </Card>
+
+        {/* Company Snippet Card */}
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-base">About The Company</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <p
+              className="text-sm text-gray-600 line-clamp-4 prose prose-sm"
+              dangerouslySetInnerHTML={{
+                __html: job.companyBio || "No Company description available",
+              }}
+            />
+
+            {job.companyWebsite && (
+              <Link
+                href={job.companyWebsite}
+                target="_blank"
+                rel="noonpener noreferrer"
+                aria-label="Visit company website (opens in new tab)"
+                className="flex items-center gap-2 text-sm font-medium text-blue-600 hover:underline"
+              >
+                <Globe className="h4 w-4" />
+                Visit Website
+              </Link>
+            )}
           </CardContent>
         </Card>
       </div>
