bfa4ccb9bccfb603653f028d4c69555eda06cd5a
elisha <elishajameel2007@gmail.com>
2026-02-10 20:01:49 +0500

    added heading and paragraph

--- src/app/(applicants)/dashboard/page.tsx
+++ src/app/(applicants)/dashboard/page.tsx
@@ -1,14 +1,24 @@
 import { logoutUserAction } from "@/app/features/auth/server/auth.action";
+import { getCurrentUser } from "@/app/features/auth/server/auth.quires";
+import { redirect } from "next/navigation";
 
 const Applicant = async () => {
-  // const user = await getCurrentUser();
-  // console.log("user data employer:", user);
+  const user = await getCurrentUser();
+  console.log("user data employer:", user);
 
-  // if (!user) return redirect("/login");
+  if (!user) return redirect("/login");
 
   return (
-    <div className="p-6">
-      <h1>Hello Applicant Dashboard.</h1>
+    <div className="space-y-8 p-6 max-w-7xl mx-auto">
+      {/* Header Section */}
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+          Hello, <span className="capitalize">{user.name}</span>
+        </h1>
+        <p className="text-gray-500">
+          Here is your daily activites and job alerts
+        </p>
+      </div>
       <button onClick={logoutUserAction}>Logout</button>
     </div>
   );
