65cd1adfbfa8bb364ef1bfca4d40b336c30273d5
elisha <elishajameel2007@gmail.com>
2026-03-01 19:14:42 +0500

    feat: applcant queries for getting data from 3 tables

--- src/app/features/applicants/components/applicant-settings-form.tsx
+++ src/app/features/applicants/components/applicant-settings-form.tsx
@@ -17,7 +17,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
   SelectContent,
---
+++ src/app/features/applicants/server/applicant.queries.ts
@@ -0,0 +1,47 @@
+import { db } from "@/config/db";
+import { applicants, resumes, users } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+
+const getApplicantProfileData = async (userId: number) => {
+  const [combinedData] = await db
+    .select({
+      user: users,
+      applicant: applicants,
+      resume: resumes,
+    })
+    .from(users)
+    .leftJoin(applicants, eq(users.id, applicants.id))
+    .leftJoin(resumes, eq(applicants.id, resumes.applicantId))
+    .where(eq(users.id, userId))
+    .limit(1); // Object Based Projection
+
+  // if no user found, combinedData will be undefined
+  if (!combinedData || !combinedData.user) return null;
+
+  const { user, applicant, resume } = combinedData;
+
+  return {
+    name: user.name,
+    email: user.email,
+    phoneNumber: user.phoneNumber ?? "",
+    avatarUrl: user.avatarUrl ?? "",
+    location: applicant?.location ?? "",
+    dateOfBirth: applicant?.dateOfBirth
+      ? new Date(applicant.dateOfBirth).toISOString().split("T")[0]
+      : "",
+    nationality: applicant?.nationality ?? "",
+    gender: applicant?.gender ?? undefined,
+    maritalStatus: applicant?.maritalStatus,
+    education: applicant?.education ?? undefined,
+    experience: applicant?.experience ?? "",
+    websiteUrl: applicant?.websiteUrl ?? "",
+    biography: applicant?.biography ?? "",
+    resumeUrl: resume?.fileUrl ?? "",
+    resumeName: resume?.fileName ?? "",
+    resumeSize: resume?.fileSize ?? undefined,
+  };
+};
+
+export type ApplicantProfileType = NonNullable<
+  Awaited<ReturnType<typeof getApplicantProfileData>>
+>;
