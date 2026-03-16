d12fb170b5ee0d52c0b0ed74aa2dc49451b48a66
elisha <elishajameel2007@gmail.com>
2026-03-16 19:41:37 +0500

    refactor: created the full navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,74 @@
+import { getCurrentUser } from "@/app/features/auth/server/auth.quires";
+import { Briefcase, LogOut } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { logoutUserAction } from "@/app/features/auth/server/auth.action";
+
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
+  return (
+    <header className="border-b bg-white sticky top-0 z-50">
+      <div className="container mx-auto max-w-7xl px-4 flex items-center justify-between">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-xl text-blue-600"
+        >
+          <Briefcase className="w-6 h-6" />
+          ElishaJob
+        </Link>
+
+        <nav className="hidden md:flex items-center gap-8 text-sm font-medium text-gray-600">
+          <Link href="/" className="hover:text-blue-600 transition-colors">
+            Home
+          </Link>
+          <Link href="/jobs" className="hover:text-blue-600 transition-colors">
+            Find Job
+          </Link>
+          <Link href="/" className="hover:text-blue-600 transition-colors">
+            Employers
+          </Link>
+        </nav>
+
+        <div className="flex items-center gap-3">
+          {!user ? (
+            <>
+              <Button variant="ghost" asChild>
+                <Link href="/login">Sign In</Link>
+              </Button>
+              <Button variant="ghost" asChild>
+                <Link href="/register">Post a Job</Link>
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button variant="outline" asChild>
+                <Link
+                  href={
+                    user.role === "employer"
+                      ? "/employer-dashboard"
+                      : "/dashboard/settings"
+                  }
+                >
+                  Dashboard
+                </Link>
+              </Button>
+
+              <form action={logoutUserAction}>
+                <Button
+                  variant="ghost"
+                  type="submit"
+                  size="icon"
+                  title="Log out"
+                >
+                  <LogOut className="w-5 h-5 text-gray-600 hover:text-red-600 transition-colors" />
+                </Button>
+              </form>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
